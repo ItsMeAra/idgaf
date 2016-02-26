7b92fb218eb0cb1adb9c87eb3db38bb2786769ee
Ara Abcarians <aabcarians@mediatemple.net>
2016-02-26 11:11:01 -0800

    Removing old folders

--- _js/idgaf.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var idgafBtn = $('a.idgafBtn');
-
-    $(document).on('click', '.idgafBtn', function ( event ) {
-        event.preventDefault();
-        document.getElementById('idgafAudio').play();
-    });
-});
